b2349b0d3f143ba461dec16bc62fd0f963efcc0b
usmanBoi <usmankith@gmail.com>
2024-06-25 02:47:50 +0500

    newupd

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,43 +1,45 @@
-"use client";
 import React from "react";
-import Head from "next/head";
 import styles from "./blog.module.css";
 import PostCard from "@/components/PostCard/PostCard";
-import { blogData } from "./blogData";
+// import { blogData } from "./blogData";
 
-const BlogPage = () => {
+const getData = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+		// cache: "no-store",
+		next: { revalidate: 3600 },
+	});
+	if (!res.ok) {
+		throw new Error("Something went wrong");
+	}
+	return res.json();
+};
+
+const BlogPage = async () => {
+	const posts = await getData();
+
 	return (
-		<>
-			{/* <Head>
-				<style jsx global>{`
-					head {
-						@keyframes appear {
-							from {
-								opacity: 0;
-								scale: 0.5;
-							}
-							to {
-								opacity: 1;
-								scale: 1;
-							}
-						}
-
-						.post {
-							animation: appear linear;
-							animation-timeline: view();
-						}
-					}
-				`}</style>
-			</Head> */}
-			<div className={styles.container}>
-				{blogData.map((blog) => (
-					<div key={blog.id} className={styles.post}>
-						<PostCard blogData={blog} />
-					</div>
-				))}
-			</div>
-		</>
+		<div className={styles.container}>
+			{posts.map((post) => (
+				<div key={post.id} className={styles.post}>
+					<PostCard post={post} />
+				</div>
+			))}
+		</div>
 	);
 };
+
+// Creating Data Manually from blogData.js
+
+// const BlogPage = () => {
+// 	return (
+// 		<div className={styles.container}>
+// 			{blogData.map((blog) => (
+// 				<div key={blog.id} className={styles.post}>
+// 					<PostCard blogData={blog} />
+// 				</div>
+// 			))}
+// 		</div>
+// 	);
+// };
 
 export default BlogPage;
--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -3,13 +3,13 @@ import styles from "./postCard.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-export const PostCard = ({ blogData }) => {
+const PostCard = ({ post }) => {
 	return (
 		<div className={styles.container}>
-			<div key={blogData.id} className={styles.upperCon}>
+			<div key={post.id} className={styles.upperCon}>
 				<div className={styles.imageCon}>
 					<Image
-						src={blogData.imageUrl}
+						src="https://images.pexels.com/photos/1060791/pexels-photo-1060791.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 						alt="my img"
 						fill
 						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
@@ -22,13 +22,44 @@ export const PostCard = ({ blogData }) => {
 				</div>
 			</div>
 			<div className={styles.lowerCon}>
-				<h2 className={styles.blogTitle}>{blogData.title}</h2>
-				<p className={styles.blogDesc}>{blogData.desc}</p>
-				<Link href={`/blog/${blogData.id}`} className={styles.artlink}>
+				<h2 className={styles.blogTitle}>{post.title}</h2>
+				<p className={styles.blogDesc}>{post.body}</p>
+				<Link href={`/blog/${post.id}`} className={styles.artlink}>
 					Read More
 				</Link>
 			</div>
 		</div>
 	);
 };
+
+// Manually creating postcards
+
+// const PostCard = ({ blogData }) => {
+// 	return (
+// 		<div className={styles.container}>
+// 			<div key={blogData.id} className={styles.upperCon}>
+// 				<div className={styles.imageCon}>
+// 					<Image
+// 						src={blogData.imageUrl}
+// 						alt="my img"
+// 						fill
+// 						sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+// 						className={styles.blogImg}
+// 						priority={false}
+// 					></Image>
+// 				</div>
+// 				<div className={styles.dateCon}>
+// 					<span className={styles.date}>10/2/2024</span>
+// 				</div>
+// 			</div>
+// 			<div className={styles.lowerCon}>
+// 				<h2 className={styles.blogTitle}>{blogData.title}</h2>
+// 				<p className={styles.blogDesc}>{blogData.desc}</p>
+// 				<Link href={`/blog/${blogData.id}`} className={styles.artlink}>
+// 					Read More
+// 				</Link>
+// 			</div>
+// 		</div>
+// 	);
+// };
 export default PostCard;
