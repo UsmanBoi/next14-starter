f035d94332824c8d7b00271f8344572c9798a63f
usmanBoi <usmankith@gmail.com>
2024-07-15 14:29:18 +0500

    laptopchanges

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,29 @@ module.exports = {
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
+		screens: {
+			sm: "640px",
+			//mobile
+			// => @media (min-width: 640px) { ... }
+			//  use it like this
+			// text-xs sm:text-base
+
+			md: "768px",
+			//tablet or mobile rotated
+			// => @media (min-width: 768px) { ... }
+
+			lg: "1024px",
+			//laptop-small
+			// => @media (min-width: 1024px) { ... }
+
+			xl: "1280px",
+			//laptop
+			// => @media (min-width: 1280px) { ... }
+
+			"2xl": "1536px",
+			//PC
+			// => @media (min-width: 1536px) { ... }
+		},
 		extend: {
 			backgroundImage: {
 				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
